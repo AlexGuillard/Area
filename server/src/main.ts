7106c30a69a8c4a33a33af89209284811e1ce35f
Benjamin <benjamin.colimard@epitech.eu>
2023-12-05 15:16:30 +0100

    feat: basic setup nest project

---
+++ server/src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs POC')
+    .setDescription('API of the POC')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }))
+  await app.listen(3000);
+}
+bootstrap();
