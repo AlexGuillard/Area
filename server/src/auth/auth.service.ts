9655b28fb52b2eb7dd9fc2fa8e07d03e9ec878b9
Benjamin <benjamin.colimard@epitech.eu>
2023-12-12 14:42:19 +0100

    fix(server): test execution

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  async signUp() {}
-  async signIn() {}
+  async signUp(params: AuthDto) {
+    return params;
+  }
+  async signIn(params: AuthDto) {
+    return params;
+  }
 }
