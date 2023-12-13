67b284da409e505bbf908be674acf1edbf681c98
Kiya971 <kiya.kebaili@epitech.eu>
2023-12-13 19:01:56 +0100

    feat: add Login page (not functionnal yet)

--- mobile/index.js
+++ mobile/index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/pages/App';
+import Login from './src/pages/Login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Login);
