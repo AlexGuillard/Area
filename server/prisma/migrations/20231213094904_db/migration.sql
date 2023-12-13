67b8ec3536e3cab5c1e9bdc91022330b9c22cb3a
Benjamin <benjamin.colimard@epitech.eu>
2023-12-13 10:52:08 +0100

    feat(server): add stringParameter in Action and Reaction tables

--- server/prisma/migrations/20231213094904_db/migration.sql
+++ server/prisma/migrations/20231213094904_db/migration.sql
@@ -27,7 +27,8 @@ CREATE TABLE "area" (
 CREATE TABLE "action" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "_serviceIdAction" INTEGER NOT NULL,
+    "stringParameter" TEXT,
+    "serviceId" INTEGER NOT NULL,
 
     CONSTRAINT "action_pkey" PRIMARY KEY ("id")
 );
@@ -36,7 +37,8 @@ CREATE TABLE "action" (
 CREATE TABLE "reaction" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "_serviceIdReaction" INTEGER NOT NULL,
+    "stringParameter" TEXT,
+    "serviceId" INTEGER NOT NULL,
 
     CONSTRAINT "reaction_pkey" PRIMARY KEY ("id")
 );
@@ -64,10 +66,10 @@ CREATE UNIQUE INDEX "area_actionId_key" ON "area"("actionId");
 CREATE UNIQUE INDEX "area_reactionId_key" ON "area"("reactionId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "action__serviceIdAction_key" ON "action"("_serviceIdAction");
+CREATE UNIQUE INDEX "action_serviceId_key" ON "action"("serviceId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "reaction__serviceIdReaction_key" ON "reaction"("_serviceIdReaction");
+CREATE UNIQUE INDEX "reaction_serviceId_key" ON "reaction"("serviceId");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "services_userId_key" ON "services"("userId");
@@ -79,16 +81,16 @@ CREATE UNIQUE INDEX "services_token_key" ON "services"("token");
 ALTER TABLE "area" ADD CONSTRAINT "area_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "action" ADD CONSTRAINT "action_id_fkey" FOREIGN KEY ("id") REFERENCES "area"("actionId") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "action" ADD CONSTRAINT "action_id_fkey" FOREIGN KEY ("id") REFERENCES "area"("reactionId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "action" ADD CONSTRAINT "action__serviceIdAction_fkey" FOREIGN KEY ("_serviceIdAction") REFERENCES "services"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "action" ADD CONSTRAINT "action_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "services"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "reaction" ADD CONSTRAINT "reaction_id_fkey" FOREIGN KEY ("id") REFERENCES "area"("reactionId") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "reaction" ADD CONSTRAINT "reaction__serviceIdReaction_fkey" FOREIGN KEY ("_serviceIdReaction") REFERENCES "services"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "reaction" ADD CONSTRAINT "reaction_serviceId_fkey" FOREIGN KEY ("serviceId") REFERENCES "services"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "services" ADD CONSTRAINT "services_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
