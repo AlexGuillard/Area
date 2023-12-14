334df270f5a1eed979e85ae2133e43d0ae69b988
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-14 10:48:35 +0100

    feat(Add Component): add the fact that edit or add is disabled if no select choosed

--- web/src/Component/Modals/Add/Add.tsx
+++ web/src/Component/Modals/Add/Add.tsx
@@ -4,6 +4,7 @@ import '../Modal.css'
 const Add = ({ onClose }) => {
     const [selectedOption1, setSelectedOption1] = useState('');
     const [selectedOption2, setSelectedOption2] = useState('');
+    const canSubmit = selectedOption1.trim() !== '' && selectedOption2.trim() !== '';
 
     const handleOption1Change = (event) => {
       setSelectedOption1(event.target.value);
@@ -15,10 +16,15 @@ const Add = ({ onClose }) => {
 
     // A modifier pour envoyer les données au serveur
     const handleSubmit = () => {
-      console.log('Selected Option 1:', selectedOption1);
-      console.log('Selected Option 2:', selectedOption2);
-  
-      onClose();
+
+      if (canSubmit) {
+        console.log('Selected Option 1:', selectedOption1);
+        console.log('Selected Option 2:', selectedOption2);
+    
+        onClose();
+      } else {
+        console.log('Can not submit caus no selected option');
+      }
     };
 
     return (
@@ -30,6 +36,7 @@ const Add = ({ onClose }) => {
           <label htmlFor="select1">Action</label>
           <select id="select1" value={selectedOption1} onChange={handleOption1Change}>
             {/* Ici utiliser une requête pour récupérer les actions possibles */}
+            <option value="">Select an Option</option>
             <option value="option1">Option 1</option>
             <option value="option2">Option 2</option>
           </select>
@@ -37,12 +44,13 @@ const Add = ({ onClose }) => {
           <label htmlFor="select2">Reaction</label>
           <select id="select2" value={selectedOption2} onChange={handleOption2Change}>
             {/* Ici utiliser une requête pour récupérer les reaction possibles */}
+            <option value="">Select an Option</option>
             <option value="optionA">Option A</option>
             <option value="optionB">Option B</option>
           </select>
 
           <div className='button-container'>
-            <button onClick={handleSubmit}>Ajouter</button>
+            <button onClick={handleSubmit} disabled={!canSubmit}>Ajouter</button>
             <button onClick={onClose} className='close-button'>Annuler</button>
           </div>
 
