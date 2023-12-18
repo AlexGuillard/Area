0b418be55cf1222c36170628c9ee7f9d7b0d3b71
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-12-18 14:57:39 +0100

    feat: add register page

--- web/src/Page/Register/register.tsx
+++ web/src/Page/Register/register.tsx
@@ -1,11 +1,47 @@
 import React from 'react';
-import './login.css';
+import { useState } from 'react';
+import './register.css';
 
-function Login() {
+function Register() {
+
+  const [textEmail, setTextEmail] = useState("");
+  const [textPassWord, setTextPassWord] = useState("");
+
+  const handleTextChangeUser = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextEmail(event.target.value);
+  };
+  const handleTextChangePassWord = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setTextPassWord(event.target.value);
+  };
+
   return (
-    <div>
+    <div className='registerPageBody'>
+        <input
+          type="text"
+          id="text-input"
+          value={textEmail}
+          onChange={handleTextChangeUser}
+          placeholder="Email"
+          className='emailRegisterArea'
+        />
+        <input
+          type="text"
+          id="text-input"
+          value={textPassWord}
+          onChange={handleTextChangePassWord}
+          placeholder="Pass word"
+          className='passwordRegisterArea'
+        />
+        <div className='connectionButon'>
+          <span>
+            Register
+          </span>
+        </div>
+        <span className='registerButon'>
+          Connection
+        </span>
     </div>
   );
 }
 
-export default Login;
+export default Register;
