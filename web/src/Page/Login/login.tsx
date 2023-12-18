c8540c2768dbe2914c6e2d79d2fca41c31c12483
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-12-18 14:57:55 +0100

    feat: add login page

--- web/src/Page/Login/login.tsx
+++ web/src/Page/Login/login.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 import './login.css';
 
 function Login() {
+
+  const [textEmail, setTextEmail] = useState("");
+  const [textPassWord, setTextPassWord] = useState("");
+
+  const handleTextChangeUser = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextEmail(event.target.value);
+  };
+  const handleTextChangePassWord = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setTextPassWord(event.target.value);
+  };
+
   return (
-    <div>
+    <div className='loginPageBody'>
+        <input
+          type="text"
+          id="text-input"
+          value={textEmail}
+          onChange={handleTextChangeUser}
+          placeholder="Email"
+          className='emailLoginArea'
+        />
+        <input
+          type="text"
+          id="text-input"
+          value={textPassWord}
+          onChange={handleTextChangePassWord}
+          placeholder="Pass word"
+          className='passwordLoginArea'
+        />
+        <div className='connectionButon'>
+          <span>
+            Connection
+          </span>
+        </div>
+        <span className='registerButon'>
+          Register
+        </span>
     </div>
   );
 }
