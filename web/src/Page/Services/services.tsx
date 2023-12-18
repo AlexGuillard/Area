7c216fa226e7c842830d3fe33ecd33b443ecded8
Alexandre Guillard <alexandre.guillard@epitech.eu>
2023-12-18 14:57:24 +0100

    feat: add service page

--- web/src/Page/Services/services.tsx
+++ web/src/Page/Services/services.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
-import './login.css';
+import './services.css';
+import User from '../../Image/User.png'
+import BackIcon from '../../Image/BackIcon.png'
+import ServiceCard from '../../Component/ServiceCard/serviceCard';
+import Google from '../../Image/Google.png'
 
-function Login() {
+function Services() {
   return (
-    <div>
+    <div className='servicesBody'>
+      <div className='servicesHeader'>
+        <div className='servicesBackArea'>
+          <img src={BackIcon} className='BackLogo'/>
+          <span className='BackTitle'>
+            Area
+          </span>
+        </div>
+        <span className='servicesTitle'>
+          Services
+        </span>
+        <img src={User} className='UserLogo'/>
+      </div>
+      <ServiceCard name="Google" status='Not Connected' user='User name' image={Google}/>
     </div>
   );
 }
 
-export default Login;
+export default Services;
