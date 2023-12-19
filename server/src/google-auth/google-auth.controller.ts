8c27ce747e2502b0b7ab44e62202f848833a50e4
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-19 17:37:32 +0100

    feat(GoogleService): now we store the accessToken from Google

--- server/src/google-auth/google-auth.controller.ts
+++ server/src/google-auth/google-auth.controller.ts
@@ -1,10 +1,13 @@
 import { GoogleOAuthGuard } from './google-oauth.guard';
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { GoogleAuthService } from './google-auth.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { ForbiddenException } from '@nestjs/common';
+import { ServiceType } from '@prisma/client';
 
 @Controller('myauth')
 export class GoogleAuthController {
-  constructor(private readonly appService: GoogleAuthService) {}
+  constructor(private readonly appService: GoogleAuthService, private prisma: PrismaService) {}
 
   @Get()
   @UseGuards(GoogleOAuthGuard)
@@ -16,6 +19,25 @@ export class GoogleAuthController {
     const token = await this.appService.googleLogin(req);
     console.log(token);
     console.log('actual', token.user.accessToken)
+    
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: token.user.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('mail not found');
+    }
+
+    await this.prisma.services.create({
+      data: {
+        token: token.user.accessToken,
+        typeService: ServiceType.GOOGLE,
+        userId: user.id,
+      },
+    });
+
     return token;
   }
 }
