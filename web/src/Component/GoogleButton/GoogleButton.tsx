73680418d8594337c84fd5a0dc3464d7e6a6c5b0
Miouzora <alexandre.franquet@epitech.eu>
2023-12-19 15:24:01 +0100

    feat(docker compose): add env var for google id and secret and server adress

--- web/src/Component/GoogleButton/GoogleButton.tsx
+++ web/src/Component/GoogleButton/GoogleButton.tsx
@@ -16,7 +16,7 @@ export default function GoogleButton() {
 
               onSuccess={async credentialResponse => {
 
-                const response = await axios.post('http://localhost:8080/auth/loginService', {
+                const response = await axios.post(process.env.SERVER_URL + '/auth/loginService', {
                   token: credentialResponse.credential
                 });
                 const data = response.data;
