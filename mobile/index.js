7a4e943cbbfada0d1c11be37955725f6b17a3d90
Kiya971 <kiya.kebaili@epitech.eu>
2023-12-19 21:55:21 +0100

    feat: add Home Page (not functional)

--- mobile/index.js
+++ mobile/index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import Login from './src/pages/Login';
+import Home from './src/pages/Home';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Login);
