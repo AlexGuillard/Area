b130e689362e31377f1aa464ad3f021972319110
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-24 12:38:54 +0100

    refactor(Gmail): remove route to send emails

--- server/src/mailing/mailing.controller.ts
+++ server/src/mailing/mailing.controller.ts
@@ -5,18 +5,4 @@ import { ApiBody } from '@nestjs/swagger';
 @Controller('mailing')
 export class MailingController {
   constructor(readonly mailingService: MailingService) {}
-  @Get('send-mail')
-  @ApiBody({
-    type: String,
-    required: true,
-  })
-  public sendMail(
-    @Query('subject') subject: string,
-    @Query('to') to: string,
-    @Query('template') template: string,
-    @Query('from') from: string,
-  ) {
-    const code = '123456';
-    this.mailingService.sendMail(subject, to, template, from, code);
-  }
 }
