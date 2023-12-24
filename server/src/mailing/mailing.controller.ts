34f37257cf77b187a07d68ce96b2fdcc02a1f193
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-24 16:09:22 +0100

    refactor(Gmail): remove useless import

--- server/src/mailing/mailing.controller.ts
+++ server/src/mailing/mailing.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { MailingService } from './mailing.service';
-import { ApiBody } from '@nestjs/swagger';
 
 @Controller('mailing')
 export class MailingController {
