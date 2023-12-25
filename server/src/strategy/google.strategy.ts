d5927839c09d71e11fe022e0723118de5660c77d
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-25 12:19:55 +0100

    refactor(GoogleService): now is the refresh token that its stored caus its the token that create other accesstoken

--- server/src/strategy/google.strategy.ts
+++ server/src/strategy/google.strategy.ts
@@ -10,6 +10,8 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: 'http://localhost:8080/myauth/google-redirect',
       scope: ['email', 'profile', 'https://mail.google.com/'], // add scope we need for example later youtube agenda etc
+      accessType: 'offline',
+      prompt: 'consent',
     });
   }
   async validate(
