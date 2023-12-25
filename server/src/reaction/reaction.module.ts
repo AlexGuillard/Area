bcf0ee717e901775e46689147ce04f6702c46840
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-25 17:59:59 +0100

    fix: imports for reaction module

--- server/src/reaction/reaction.module.ts
+++ server/src/reaction/reaction.module.ts
@@ -5,11 +5,12 @@ import { MeService } from '../me/me.service';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AboutService } from '../about/about.service';
-import { MailingModule } from 'src/mailing/mailing.module';
-import { MailingService } from 'src/mailing/mailing.service';
+import { MailingModule } from '../mailing/mailing.module';
+import { MailingService } from '../mailing/mailing.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [HttpModule, PrismaModule, MailingModule],
+  imports: [HttpModule, PrismaModule, MailingModule, ConfigModule.forRoot()],
   providers: [ReactionService, MeService, AboutService, MailingService],
   controllers: [ReactionController],
 })
