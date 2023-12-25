0cf2b4dcbc3ed853f388f859ffcda1f924f19d53
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-25 11:57:11 +0100

    feat(Gmail): update reaction's module to use email

--- server/src/reaction/reaction.module.ts
+++ server/src/reaction/reaction.module.ts
@@ -5,10 +5,12 @@ import { MeService } from '../me/me.service';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AboutService } from '../about/about.service';
+import { MailingModule } from 'src/mailing/mailing.module';
+import { MailingService } from 'src/mailing/mailing.service';
 
 @Module({
-  imports: [HttpModule, PrismaModule],
-  providers: [ReactionService, MeService, AboutService],
+  imports: [HttpModule, PrismaModule, MailingModule],
+  providers: [ReactionService, MeService, AboutService, MailingService],
   controllers: [ReactionController],
 })
 export class ReactionModule {}
