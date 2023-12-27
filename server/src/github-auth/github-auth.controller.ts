c056346a6b4ef01bd95f342e83c32b98f79a084b
Pablodeibar <pablo.de-ibar@epitech.eu>
2023-12-27 11:03:24 +0100

    feat(GithubService): use github dto on github controller

--- server/src/github-auth/github-auth.controller.ts
+++ server/src/github-auth/github-auth.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Req, UseGuards, Res } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GithubDto } from './dto/github.dto';
 
 @Controller('auth')
 export class GithubAuthController {
@@ -8,7 +9,7 @@ export class GithubAuthController {
   @Get()
   @UseGuards(AuthGuard('github'))
   async login() {
-    //
+    // initiates the GitHub OAuth2 login flow
   }
 
   @Get('callback')
@@ -16,7 +17,19 @@ export class GithubAuthController {
   async authCallback(@Req() req, @Res() res) {
     const user = req.user;
 
-    console.log(user);
+    const githubOAuthDto = new GithubDto();
+    githubOAuthDto.id = user.id;
+    githubOAuthDto.displayName = user.displayName;
+    githubOAuthDto.username = user.username;
+    githubOAuthDto.profileUrl = user.profileUrl;
+    githubOAuthDto.photos = user.photos;
+    githubOAuthDto.provider = user.provider;
+    githubOAuthDto._raw = user._raw;
+    githubOAuthDto._json = user._json;
+    githubOAuthDto.accessToken = user.accessToken;
+
+    console.log('GitHub User Data:', githubOAuthDto);
+
     return res.redirect('http://localhost:8081/Area');
   }
 }
