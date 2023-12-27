96a759a6f70f374aedb20f581de46c3eb2ff1ea2
Benjamin <benjamin.colimard@epitech.eu>
2023-12-27 12:57:45 +0100

    refactor: run lint

--- server/src/discord/discord.service.ts
+++ server/src/discord/discord.service.ts
@@ -6,61 +6,62 @@ import { ServiceType } from '@prisma/client';
 
 @Injectable()
 export class DiscordService {
-    constructor(
-      private prisma: PrismaService,
-    ) {}
-    async discordLogin(@Res() req: any) {
-        const url = 'https://discord.com/api/oauth2/authorize?client_id=1189230397108793416&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fdiscord%2Fcallback&scope=identify+email'
-        req.redirect(url);
+  constructor(private prisma: PrismaService) {}
+  async discordLogin(@Res() req: any) {
+    const url =
+      'https://discord.com/api/oauth2/authorize?client_id=1189230397108793416&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fdiscord%2Fcallback&scope=identify+email';
+    req.redirect(url);
+  }
+
+  async discordCallback(@Request() req: any, @Res() res: any) {
+    if (!req.query.code) throw new Error('NoCodeProvided');
+    const { code } = req.query;
+    const params = new URLSearchParams({
+      client_id: process.env.DISCORD_CLIENT_ID,
+      client_secret: process.env.DISCORD_CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    });
+
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Accept-Encoding': 'application/x-www-form-urlencoded',
+    };
+
+    const response = await axios.post(
+      'https://discord.com/api/oauth2/token',
+      params,
+      {
+        headers,
+      },
+    );
+    const userResponse = await axios.get('https://discord.com/api/users/@me', {
+      headers: {
+        Authorization: `${response.data.token_type} ${response.data.access_token}`,
+      },
+    });
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: userResponse.data.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException(
+        'mail not found. Please register with the same email as the one you are connected',
+      );
     }
 
-    async discordCallback(@Request() req: any, @Res() res: any) {
-        if (!req.query.code) throw new Error('NoCodeProvided');
-        const { code } = req.query;
-        const params = new URLSearchParams({
-          client_id: process.env.DISCORD_CLIENT_ID,
-          client_secret: process.env.DISCORD_CLIENT_SECRET,
-          grant_type: 'authorization_code',
-          code,
-          redirect_uri: process.env.DISCORD_REDIRECT_URI
-        });
+    await this.prisma.services.create({
+      data: {
+        token: response.data.access_token,
+        refreshToken: response.data.refresh_token,
+        typeService: ServiceType.DISCORD,
+        userId: user.id,
+      },
+    });
 
-        const headers = {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Accept-Encoding': 'application/x-www-form-urlencoded'
-        };
-
-        const response = await axios.post(
-          'https://discord.com/api/oauth2/token',
-          params,
-          {
-            headers
-          }
-        );
-        const userResponse = await axios.get('https://discord.com/api/users/@me', {
-            headers: {
-                Authorization: `${response.data.token_type} ${response.data.access_token}`
-            }
-        });
-        const user = await this.prisma.user.findUnique({
-          where: {
-            email: userResponse.data.email,
-          },
-        });
-    
-        if (!user) {
-          throw new ForbiddenException('mail not found. Please register with the same email as the one you are connected');
-        }
-    
-        await this.prisma.services.create({
-          data: {
-            token: response.data.access_token,
-            refreshToken: response.data.refresh_token,
-            typeService: ServiceType.DISCORD,
-            userId: user.id,
-          },
-        });
-    
-        res.redirect('http://localhost:8081/Area');
-    }
+    res.redirect('http://localhost:8081/Area');
+  }
 }
