1b42f15841af089dfc4bf1656b443e9ffc4046e9
Benjamin <benjamin.colimard@epitech.eu>
2024-01-04 16:45:41 +0100

    test: fix compil

--- server/src/action/time/time.service.spec.ts
+++ server/src/action/time/time.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TimeService } from './time.service';
+import { HttpModule } from '@nestjs/axios';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ActionModule } from '../action.module';
+import { ActionService } from '../action.service';
+import { MeService } from '../../me/me.service';
+import { PrismaModule } from '../../prisma/prisma.module';
+import { AboutService } from '../..//about/about.service';
 
 describe('TimeService', () => {
   let service: TimeService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TimeService],
+      providers: [TimeService, ActionService, MeService, AboutService],
+      imports: [EventEmitterModule.forRoot(), ActionModule, HttpModule, PrismaModule],
     }).compile();
 
     service = module.get<TimeService>(TimeService);
