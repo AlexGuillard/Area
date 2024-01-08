f41ad73214ef5af6a98d26a275fbeb5c84b29b0a
Alexandre Guillard <alexandre.guillard@epitech.eu>
2024-01-08 13:30:33 +0100

    feat: add state services to the services card

--- web/src/Page/Services/services.tsx
+++ web/src/Page/Services/services.tsx
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './services.css';
 import { useNavigate} from 'react-router-dom';
-import Cookies from 'js-cookie';import
-User from '../../Image/User.png'
+import Cookies from 'js-cookie';
+import axios from 'axios';
+import User from '../../Image/User.png'
 import BackIcon from '../../Image/BackIcon.png'
 import ServiceCard from '../../Component/ServiceCard/serviceCard';
 import Google from '../../Image/Google.png'
@@ -13,11 +14,47 @@ function Services() {
 
   const navigate = useNavigate()
 
+  interface ServiceItem {
+    typeService: string;
+  }
+
+  const [listServices, setListServices] = useState<ServiceItem[]>()
+  const [stateGoogle, setStateGoogle] = useState("Not Connected")
+  const [stateDiscord, setStateDiscord] = useState("Not Connected")
+  const [stateGithub, setStateGithub] = useState("Not Connected")
+
+  const handleCallServicesList = () => {
+    const storedToken = Cookies.get('token');
+    axios.get(process.env.REACT_APP_SERVER_URL + "/" + storedToken + "/services")
+      .then(response => {
+        setListServices(response.data)
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+
   useEffect(() => {
     const storedToken = Cookies.get('token');
-    if (storedToken == "undefined")
+    if (storedToken === "undefined")
       navigate("/")
-  }, [navigate]);
+    if (listServices === undefined) {
+      handleCallServicesList()
+    } else  {
+      let i = 0
+      while (listServices[i]) {
+        if (listServices[i].typeService === "GOOGLE") {
+          setStateGoogle("Connected")
+        } else if (listServices[i].typeService === "DISCORD") {
+          setStateDiscord("Connected")
+        } else if (listServices[i].typeService === "GITHUB") {
+          setStateGithub("Connected")
+        }
+        i = i + 1
+      }
+    }
+  }, [navigate, listServices]);
 
   return (
     <div className='servicesBody'>
@@ -34,9 +71,9 @@ function Services() {
         <img src={User} className='UserLogo' alt="user logo"/>
       </div>
       <div className='servicesCardArea'>
-        <ServiceCard name="Google" status='Not Connected' user='User name' image={Google} link='http://localhost:8080/myauth/google-redirect'/>
-        <ServiceCard name="Discord" status='Not Connected' user='User name' image={Discord} link='http://localhost:8080/services/discord/login'/>
-        <ServiceCard name="Github" status='Not Connected' user='User name' image={Github} link='http://localhost:8080/auth/github'/>
+        <ServiceCard name="Google" status={stateGoogle} user='' image={Google} link='http://localhost:8080/myauth/google-redirect'/>
+        <ServiceCard name="Discord" status={stateDiscord} user='' image={Discord} link='http://localhost:8080/services/discord/login'/>
+        <ServiceCard name="Github" status={stateGithub} user='' image={Github} link='http://localhost:8080/auth/github'/>
       </div>
     </div>
   );
