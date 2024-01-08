96b03feccf242716b1b222ed866fe7f6654f6033
Miouzora <alexandre.franquet@epitech.eu>
2024-01-08 17:01:51 +0100

    test(mobile): add test to check api call

--- mobile/__tests__/Login.test.tsx
+++ mobile/__tests__/Login.test.tsx
@@ -11,7 +11,40 @@ import {it} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import axios from 'axios';
 
 it('renders correctly', () => {
   renderer.create(<Login />);
+});
+
+function random_string(length: number) {
+  let result = '';
+  const characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+it('connect to back', async () => {
+  const mail = random_string(10) + '@' + random_string(10) + '.com';
+  const password = 'test';
+  // use axios to connect to back
+  const data = {
+    mail: mail,
+    password: password,
+  };
+
+  // axios
+  await axios
+    .post('http://localhost:8080/auth/signup', data)
+    .then(() => {
+      console.log('success');
+    })
+    .catch(error => {
+      console.error(error.response);
+    });
 });
