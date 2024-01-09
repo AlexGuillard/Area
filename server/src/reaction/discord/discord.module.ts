d51a63529b9fa88d2a62ead5345938d23ef5ae7d
Benjamin <benjamin.colimard@epitech.eu>
2024-01-09 11:33:21 +0100

    fix: remove controller

--- server/src/reaction/discord/discord.module.ts
+++ server/src/reaction/discord/discord.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DiscordService } from './discord.service';
-import { MeService } from 'src/me/me.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
-import { DiscordController } from './discord.controller';
 
 @Module({
   providers: [DiscordService],
   imports: [PrismaModule],
-  controllers: [DiscordController],
 })
 export class DiscordModule {}
