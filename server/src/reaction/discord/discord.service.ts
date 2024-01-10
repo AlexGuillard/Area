d958a97a5dab5ae426a4216b3c4f6190a4a3d991
Benjamin <benjamin.colimard@epitech.eu>
2024-01-10 14:05:36 +0100

    fix: lint

--- server/src/reaction/discord/discord.service.ts
+++ server/src/reaction/discord/discord.service.ts
@@ -6,104 +6,135 @@ import { PrismaService } from '../../prisma/prisma.service';
 
 @Injectable()
 export class DiscordService {
-    constructor(
-        private eventEmitter: EventEmitter2,
-        private prismaService: PrismaService,
-    ) {
-    this.eventEmitter.on("sendMessageDiscord.struct", (struct: DiscordSendMessageDto) => {
+  constructor(
+    private eventEmitter: EventEmitter2,
+    private prismaService: PrismaService,
+  ) {
+    this.eventEmitter.on(
+      'sendMessageDiscord.struct',
+      (struct: DiscordSendMessageDto) => {
         struct.name_server = 'string';
         struct.name_channel = 'string';
         struct.content = 'string';
-      })
-      this.eventEmitter.on("send message user.struct", (struct: DiscordSendMessageUserDto) => {
-          struct.id_user = 'string';
-          struct.content = 'string';
-      })
-    }
+      },
+    );
+    this.eventEmitter.on(
+      'send message user.struct',
+      (struct: DiscordSendMessageUserDto) => {
+        struct.id_user = 'string';
+        struct.content = 'string';
+      },
+    );
+  }
 
-    @OnEvent('sendMessageDiscord')
-    async sendMessage(struct: DiscordSendMessageDto, token: string) {
-        void token;
-        await axios.get('https://discord.com/api/v10/users/@me/guilds', {
-          headers: {
-            Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-          },
-        }).then((res) => {
-          res.data.forEach(async element => {
-            if (element.name === struct.name_server) {
-              await axios.get(`https://discord.com/api/v10/guilds/${element.id}/channels`, {
-                headers: {
-                  Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+  @OnEvent('sendMessageDiscord')
+  async sendMessage(struct: DiscordSendMessageDto, token: string) {
+    void token;
+    await axios
+      .get('https://discord.com/api/v10/users/@me/guilds', {
+        headers: {
+          Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+        },
+      })
+      .then((res) => {
+        res.data.forEach(async (element) => {
+          if (element.name === struct.name_server) {
+            await axios
+              .get(
+                `https://discord.com/api/v10/guilds/${element.id}/channels`,
+                {
+                  headers: {
+                    Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+                  },
                 },
-              }).then((res) => {
-                res.data.forEach(async element => {
+              )
+              .then((res) => {
+                res.data.forEach(async (element) => {
                   if (element.name === struct.name_channel) {
-                    await axios.post(`https://discord.com/api/v10/channels/${element.id}/messages`, {
-                      content: struct.content,
-                    }, {
-                      headers: {
-                        Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
-                      },
-                    }).catch((err) => {
-                      throw Error(err);
-                    });
+                    await axios
+                      .post(
+                        `https://discord.com/api/v10/channels/${element.id}/messages`,
+                        {
+                          content: struct.content,
+                        },
+                        {
+                          headers: {
+                            Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
+                          },
+                        },
+                      )
+                      .catch((err) => {
+                        throw Error(err);
+                      });
                   }
                 });
               })
               .catch((err) => {
-                  throw Error(err);
+                throw Error(err);
               });
-            }
-          });
-        }).catch((err) => {
-            throw Error(err);
+          }
         });
+      })
+      .catch((err) => {
+        throw Error(err);
+      });
+  }
+
+  async findService(token: string) {
+    const userId = await this.prismaService.user.findFirst({
+      where: {
+        randomToken: token,
+      },
+    });
+
+    if (!userId) {
+      throw new Error('User not found, or discord services not configured');
     }
 
-    async findService(token: string) {
-      const userId = await this.prismaService.user.findFirst({
-        where: {
-          randomToken: token,
+    const service = await this.prismaService.services.findUnique({
+      where: {
+        UniqueUserService: {
+          userId: userId.id,
+          typeService: 'DISCORD',
         },
-      });
+      },
+    });
+    return service;
+  }
 
-      if (!userId) {
-        throw new Error('User not found, or discord services not configured');
-      }
-
-      const service = await this.prismaService.services.findUnique({
-        where: {
-          UniqueUserService: {
-            userId: userId.id,
-            typeService: "DISCORD",
-          },
-        },
-      });
-      return service;
-    }
-
-    @OnEvent('send message user')
-    async sendMessageUser(struct: DiscordSendMessageUserDto, token: string) {
-        const service = await this.findService(token);
-        const res = await axios.post(`https://discord.com/api/v10/users/@me/channels`, {
+  @OnEvent('send message user')
+  async sendMessageUser(struct: DiscordSendMessageUserDto, token: string) {
+    const res = await axios
+      .post(
+        `https://discord.com/api/v10/users/@me/channels`,
+        {
           recipient_id: struct.id_user,
-        }, {
+        },
+        {
           headers: {
             Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
             'Content-Type': 'application/json',
           },
-        }).catch((err) => {
-          throw Error("error before: " + err);
-        });
-        console.log(res.data);
-        await axios.post(`https://discord.com/api/v10/channels/${res.data.id}/messages`, {
+        },
+      )
+      .catch((err) => {
+        throw Error('error before: ' + err);
+      });
+    console.log(res.data);
+    await axios
+      .post(
+        `https://discord.com/api/v10/channels/${res.data.id}/messages`,
+        {
           content: struct.content,
-        }, {
+        },
+        {
           headers: {
             Authorization: `Bot ${process.env.DISCORD_BOT_TOKEN}`,
           },
-        }).catch((err) => {
-          throw Error(err);
-        });
-    }
+        },
+      )
+      .catch((err) => {
+        throw Error(err);
+      });
+  }
 }
