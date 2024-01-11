913df94a480920330e8bccceea240d078c56efae
Pablodeibar <pablo.de-ibar@epitech.eu>
2024-01-11 15:20:20 +0100

    fix(SpotifyService): TUX

--- server/src/action/openweather/openweather.service.spec.ts
+++ server/src/action/openweather/openweather.service.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OpenweatherService } from './openweather.service';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ActionModule } from '../action.module';
 
 describe('OpenweatherService', () => {
   let service: OpenweatherService;
@@ -7,6 +9,7 @@ describe('OpenweatherService', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [OpenweatherService],
+      imports: [EventEmitterModule.forRoot(), ActionModule],
     }).compile();
 
     service = module.get<OpenweatherService>(OpenweatherService);
