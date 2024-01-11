1a34fa90302dcb1990337f234fb3b6becdd2b0fc
Pablodeibar <pablo.de-ibar@epitech.eu>
2024-01-11 11:01:20 +0100

    fix: CI

--- server/src/action/spotify/spotify.module.ts
+++ server/src/action/spotify/spotify.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SpotifyService } from './spotify.service';
 import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   providers: [SpotifyService, PrismaService],
   controllers: [],
+  imports: [PrismaModule],
 })
 export class SpotifyModule {}
