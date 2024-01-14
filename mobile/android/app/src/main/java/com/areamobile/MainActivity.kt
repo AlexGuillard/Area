24ce1feb5f88ab18742473ea8482d3cf23bbf2aa
Benjicatch <benjamin.colimard@epitech.eu>
2024-01-14 16:54:50 +0100

    fix: change name mobile

--- mobile/android/app/src/main/java/com/areamobile/MainActivity.kt
+++ mobile/android/app/src/main/java/com/areamobile/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.mobile
+package com.example.mobile
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- mobile/android/app/src/main/java/com/areamobile/MainApplication.kt
+++ mobile/android/app/src/main/java/com/areamobile/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.mobile
+package com.example.mobile
 
 import android.app.Application
 import com.facebook.react.PackageList
